d8292bbc47637f57cfbecc25d400bb21298454cd
YAVAS <YAVAS@DESKTOP-O80KOSR>
2023-10-01 20:48:31 +0300

    CategoryRepository oluşturuldu.

---
+++ StokTakip.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,41 @@
+﻿using StokTakip.DataAccess.Concrete.Contexts;
+using StokTakip.DataAccess.Concrete.EfCore;
+using StokTakip.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Business.Concrete
+{
+    public class CategoryManager
+    {
+        private readonly CategoryRepository _categoryRepository;
+
+        public CategoryManager(StokTakipDbContext context)
+        {
+            _categoryRepository = new CategoryRepository(context);
+        }
+
+        public bool Add(Category category)
+        {
+            int result = _categoryRepository.Add(category);
+
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Delete(Category category)
+        {
+            int result = _categoryRepository.Delete(category);
+
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ StokTakip.DataAccess/Concrete/EfCore/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using StokTakip.DataAccess.Concrete.Contexts;
+using StokTakip.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.DataAccess.Concrete.EfCore
+{
+    public class CategoryRepository
+    {
+        private readonly StokTakipDbContext _context;
+
+        public CategoryRepository(StokTakipDbContext context)
+        {
+            _context = context;
+        }
+
+        public int Add(Category category)
+        {
+            _context.Categories.Add(category);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Category category)
+        {
+            _context.Categories.Update(category);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Category category)
+        {
+            _context.Categories.Remove(category);
+            return _context.SaveChanges();
+        }
+
+        public List<Category> GetAll()
+        {
+            return _context.Categories.ToList();
+        }
+    }
+}
